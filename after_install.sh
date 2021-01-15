74a341cf53668f678eba2f067069ca42a7bdcd4e
collinsctk@qytang.com <collinsctk@qytang.com>
2021-01-15 09:32:30 +0800

    添加before_install

--- after_install.sh
+++ after_install.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-# 安装python3
-yum install -y python3
-
-# 安装python 模块
-pip3 install flask
-pip3 install requests
-
-# 安装与配置uwsgi
-yum install -y gcc python3-devel
-pip3 install uwsgi
-ln -s /usr/local/bin/uwsgi /usr/bin/uwsgi
+# 配置uwsgi
 cp /aws_flask/config_files/qytang.service /etc/systemd/system/qytang.service
 systemctl start qytang.service
 systemctl enable qytang.service
 
-# 安装与配置NGINX
-amazon-linux-extras install -y nginx1.12
+# 配置NGINX
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf-bak
 cp /aws_flask/config_files/nginx.conf /etc/nginx/nginx.conf
 systemctl start nginx
---
+++ before_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 安装python3
+yum install -y python3
+
+# 安装python 模块
+pip3 install flask
+pip3 install requests
+
+# 安装uwsgi
+yum install -y gcc python3-devel
+pip3 install uwsgi
+ln -s /usr/local/bin/uwsgi /usr/bin/uwsgi
+
+# 安装NGINX
+amazon-linux-extras install -y nginx1.12
+
